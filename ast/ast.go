633d816f667c3702a427d1adbeaae49892dffb4a
Souehda <39994816+Souehda@users.noreply.github.com>
2025-01-02 16:37:22 +0100

    Write first version for the skeleton of our AST

---
+++ ast/ast.go
@@ -0,0 +1,55 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Implements the Statement interface
+type LetStatement struct {
+	Token token.Token // Let token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+/*
+Here Identifier struct implements the Expression interface even though all identifiers doesn't produce value.
+To keep things simple, we want to implement this interface to deal with the following kind of case :
+
+let result = aValueProducingIdentifier;
+
+Here, aValueProducingIdentifier is a pointer to a function that return a value.
+*/
+type Identifier struct {
+	Token token.Token // Ident token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
